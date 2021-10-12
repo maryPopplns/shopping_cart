06f89aed2220689c907a8701467210fa3adebac7
Spencer Knight <spencer.knightusn@gmail.com>
2021-10-12 10:46:03 -0700

    added back icon to planet_modal

--- src/components/planet_modal/PlanetModal.js
+++ src/components/planet_modal/PlanetModal.js
@@ -3,10 +3,14 @@ import './planetModal.css';
 
 export default function PlanetModal() {
   let { planetId } = useParams();
+  function hidden() {
+    document.body.style.overflow = 'hidden';
+  }
   return (
     <div id='planet_modal'>
+      <i className='far fa-arrow-alt-circle-left planet_model_back_arrow'></i>
       <div>{planetId}</div>
-      {(document.body.style.overflow = 'hidden')}
+      {hidden()}
     </div>
   );
 }
